bee82e1a5f4287b7a3835b259e4f84f6d4daa0fb
beatricerocha <beatricerocha.it@gmail.com>
2021-12-02 14:38:10 -0300

    alterado nome do arquivo para 4

--- dia-4/exercicios/Exercicio01.java
+++ dia-4/exercicios/Exercicio01.java
@@ -2,7 +2,7 @@ package exercicios;
 
 import java.util.Scanner;
 
-public class Exercicio03 {
+public class Exercicio01 {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
     float n1, n2;

8ee83d9962ce50f3ff1d9518c496973a820c91ce
beatricerocha <beatricerocha.it@gmail.com>
2021-12-03 12:05:18 -0300

    exercicio 1 aula 5

---
+++ dia-5/exercicio/Exercicio01.java
@@ -0,0 +1,16 @@
+package exercicio;
+
+class Exercicio01 {
+
+  public static void main(String[] args) {
+    int cont;
+    cont = 100;
+
+    while (cont < 200) {
+      System.out.print(cont + " ");
+      cont++;
+    }
+    System.out.println(cont);
+  }
+
+}

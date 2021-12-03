c5cf62db2fb5ff32ebc2900984a365a2ef3eceaf
beatricerocha <beatricerocha.it@gmail.com>
2021-12-03 12:04:56 -0300

    exemplo 2 aula 5

---
+++ dia-5/exemplo/Exemplo02.java
@@ -0,0 +1,14 @@
+package exemplo;
+
+public class Exemplo02 {
+  public static void main(String[] args) {
+    int cont;
+    cont = 1;
+
+    while (cont < 10) {
+      System.out.println(cont);
+      cont++;
+    }
+    System.out.println("Final: " + cont);
+  }
+}

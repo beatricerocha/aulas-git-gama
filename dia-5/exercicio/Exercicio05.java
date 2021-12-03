8d2d5c0927329e911683722dfe7ce3e78da77f3a
beatricerocha <beatricerocha.it@gmail.com>
2021-12-03 15:27:57 -0300

    Finalizado exercicio 5 aula 5

---
+++ dia-5/exercicio/Exercicio05.java
@@ -0,0 +1,24 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    final int TOTAL_NUMEROS = 10;
+    int numero, pares;
+
+    pares = 0;
+    for (int i = 1; i <= TOTAL_NUMEROS; i++) {
+
+      System.out.printf("Digite %d° número: ", i);
+      numero = entrada.nextInt();
+      if (numero % 2 == 0) {
+        pares++;
+      }
+    }
+    System.out.println("O total de pares é: " + pares);
+    System.out.println("O total de impares é: " + (TOTAL_NUMEROS - pares));
+    entrada.close();
+  }
+}

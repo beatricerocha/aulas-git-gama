02567e758c84b3665359bc1298312002e36e50e0
beatricerocha <beatricerocha.it@gmail.com>
2021-12-03 13:17:47 -0300

    akterado exercicio 3 aula 5

--- dia-5/exercicio/Exercicio03.java
+++ dia-5/exercicio/Exercicio03.java
@@ -6,17 +6,21 @@ public class Exercicio03 {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
     int valorDigitado, cont;
-    System.out.println("Digite um numero: ");
+
+    System.out.print("Digite um numero: ");
     valorDigitado = entrada.nextInt();
 
     cont = 1;
 
     while (cont <= valorDigitado) {
-
-      System.out.printf("%d ", cont);
+      int proximoNumero = cont * 2;
+      if (proximoNumero <= valorDigitado) {// existirá outro num depois
+        System.out.print(cont + ",");
+      } else {
+        System.out.print(cont);
+      }
       cont *= 2;// cont = cont * 2
     }
-
     entrada.close();
   }
 }

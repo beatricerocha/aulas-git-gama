bfafcbc1171d807cfc7602896c4e0b8fbdfc9fb9
beatricerocha <beatricerocha.it@gmail.com>
2021-12-06 15:30:27 -0300

    Exercicio 1 dia 6 finalizado

--- dia-6/exercicios/Exercicio01.java
+++ dia-6/exercicios/Exercicio01.java
@@ -2,20 +2,20 @@ package exercicios;
 
 public class Exercicio01 {
   public static void main(String[] args) {
-    boolean resultado = false;
-    resultado = verificaPar(11);
-    if (resultado) {
-      System.out.println("Número é par");
+    int numero = 11;
+
+    if (verificaPar(11)) {
+      System.out.println("Número " + numero + " é par");
     } else {
-      System.out.println("Número é impar");
+      System.out.println("Número " + numero + " impar");
     }
   }
 
   public static boolean verificaPar(int numero) {
     if (numero % 2 == 0) {
       return true;
-    } else {
-      return false;
     }
+    return false;
+
   }
 }

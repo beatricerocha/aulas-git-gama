4f822097fc62f918ff40fc3925413a855e1b8f26
beatricerocha <beatricerocha.it@gmail.com>
2021-12-07 14:03:10 -0300

    Exercicio 2 finalizado dia 7

---
+++ dia-7/exercicio02/AppEbook.java
@@ -0,0 +1,23 @@
+package exercicio02;
+
+public class AppEbook {
+  public static void main(String[] args) {
+
+    Ebook livro = new Ebook("Hunger games", "S. Collins", 300);
+
+    // livro.retrocederPagina();
+    // System.out.println("Página atual: " + livro.exibirPagina());
+
+    // livro.irParaPagina(350);
+    // System.out.println("Página atual: " + livro.exibirPagina());
+
+    // livro.avancarPagina();
+    // System.out.println("Página atual: " + livro.exibirPagina());
+
+    // livro.retrocederPagina();
+    // System.out.println("Página atual: " + livro.exibirPagina());
+
+    livro.mostrarCapa();
+
+  }
+}

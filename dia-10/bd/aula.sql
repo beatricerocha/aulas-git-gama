6d842389c77e6ef9305b3aa9855b2ea1f545b6c5
beatricerocha <beatricerocha.it@gmail.com>
2021-12-13 21:00:37 -0300

    Aula 10 finalizada

--- dia-10/bd/aula.sql
+++ dia-10/bd/aula.sql
@@ -1,41 +1,83 @@
--- comentario SQL não faz diferença MAIÚSCULA e minuscula
--- para nomes compostos usa underline
--- drop table cliente;
+-- isso é um comentário
 
--- marcar bd como padão 
-use bd_itau;
--- criar BASE DE DADOS
+-- criar uma nova "Base de Dados"
 create database bd_itau;
-desc cliente; -- descreve  table cliente
-drop table cliente;
-create table cliente(
+
+-- marcar o BD como padrão
+use bd_itau;
+
+-- criar uma nova tabela no BD
+-- [nome coluna][tipo de dados][qualificadores]
+-- not null = estacoluna não pode ser deixada em branco
+-- auto_increment = numeração será gerada pelo próprio BD (1, 2, 3, 4, ...)
+-- varchar(x) = campo de texto com tamanho máximo x
+-- constraint é uma regra (restrição)
+-- primary key = chave primária (identificador na tabela), não pode repetir
+
+create table cliente ( 
 	codigo integer not null auto_increment,
     nome varchar(120) not null,
     telefone varchar(20),
-    constraint pk_cliente primary key (codigo)
+    constraint pk_cliente primary key(codigo)
 );
 
-insert into cliente (codigo, nome, telefone) values (111, 'Carlos Augusto', '(11) 9999-9999');
-insert into cliente (codigo, nome, telefone) values (112, 'Fernando Augusto', '(11) 9999-9992');
-insert into cliente (codigo, nome, telefone) values (113, 'José Augusto', '(11) 9999-9993');
-insert into cliente (codigo, nome, telefone) values (null, 'José Fernandes', '(11) 9999-9994');
-select * from cliente;
-select nome, telefone from cliente;
+-- CUIDADO!!!! apaga a estrutura da tabela e TODOS os dados junto
+-- drop table cliente;
 
-select nome, telefone from cliente where codigo =112;
-delete from cliente where codigo =114;
+-- descrever os detalhes da tabela
+desc cliente;
 
--- atualizar dados 
-update cliente set telefone = '(11) 9999-4999' where codigo =112;
+-- inserir dados na tabela
+insert into cliente (codigo, nome, telefone) values (null, 'Carlos Augusto', '(11) 98765-4321');
+insert into cliente (codigo, nome, telefone) values (null, 'Amanda Souza', '(11) 94444-4321');
+insert into cliente (codigo, nome, telefone) values (null, 'Ricardo Santos', '(12) 91111-2222');
+insert into cliente (codigo, nome, telefone) values (null, 'Felipe Prado', '(19) 87878-4446');
 
-create table veiculo (
+-- buscar as informações no banco de dados
+-- select [quais colunas] from [qual tabela] where [qual condição]
+
+-- buscar todas as informações de todos os clientes
+select *
+from cliente;
+
+-- buscar o nome e o telefone do cliente com código 2
+select nome, telefone -- busque apenas nome e telefone
+from cliente          -- na tabela cliente
+where codigo = 2;     -- onde o código seja igual a 2
+
+-- apagando dados
+-- apagar o cliente que tem código 3
+delete from cliente where codigo > 3;
+
+-- atualizar dados
+update cliente
+set telefone = '(21)12345-1234'
+where codigo = 3;
+
+-- criar tabela veiculo
+-- foreign key = chave extrangeira, ligar coma outra tabela
+-- references = indica com qual tabela e campo a ligação é feita
+
+create table veiculo(
 	codigo integer not null auto_increment,
     marca varchar(20),
-    proprietario integer, 
-    constraint pk_veiculo primary key (codigo), 
-    constraint fk_veiculo foreign key (proprietario) references cliente(codigo));
+    modelo varchar(40),
+    proprietario integer,
+	constraint pk_veiculo primary key(codigo),
+    constraint fk_veiculo_cliente foreign key (proprietario) references cliente(codigo)
+);
 
-insert into cliente (codigo, marca, proprietario) values (111, 'Carlos Augusto', '(11) 9999-9999');
+-- inserir dados de veiculos
+insert into veiculo values (null, 'VW', 'Gol', 1);
+insert into veiculo values (null, 'Chevrolet', 'Astra', 1);
+insert into veiculo values (null, 'BMW', 'X1', 2);
 
-insert into veiculo values (null, 'BMW', 'X', 111);
-select * from veiculo;
+-- buscar todos os dados de todos os veículos
+select *
+from veiculo;
+
+-- quais os nomes dos proprietarios de cada veiculo?
+-- inner join = junção das tabelas onde os caompos são iguais
+
+select marca, modelo, nome
+from veiculo inner join cliente on veiculo.proprietario = cliente.codigo;
